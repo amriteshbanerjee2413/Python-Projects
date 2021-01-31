30f60f0e8348db044723c296f88922b4aec46dc8
amriteshbanerjee2413 <67334509+amriteshbanerjee2413@users.noreply.github.com>
2021-01-31 19:34:16 +0400

    Update Madlib.py

--- Madlib.py
+++ Madlib.py
@@ -1,2 +1,11 @@
 #Variables and storing user data
 
+
+color = input("Enter a color: ")
+pluralNoun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ")
+
+#Printing the code
+print("Roses are", color)
+print(pluralNoun + " are blue")
+print("I love", celebrity)
